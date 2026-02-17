b91f64ac7f631e13eddedbc222775d0f4f70e518
shivani2965 <shivanidevanekar2965@gmail.com>
2026-02-17 23:59:54 +0530

    day n

--- Basics/BasicHashing/BasicHashing.java
+++ Basics/BasicHashing/BasicHashing.java
@@ -1,12 +1,12 @@
-import java.util.*;
+    import java.util.*;
 
 public class BasicHashing {
 
     public static void main(String[] args) {
 
-        /* -----------------------------
-           1. Hashing Numbers (Array)
-           ----------------------------- */
+        // /* -----------------------------
+        //    1. Hashing Numbers (Array)
+        //    ----------------------------- */
 
         int[] arr = {1, 2, 3, 1, 3, 2, 1};
         int max = 10; // range of numbers
@@ -23,54 +23,77 @@ public class BasicHashing {
             System.out.println("Frequency of " + q + " = " + hash[q]);
         }
 
-        System.out.println("----------------------------");
+        // System.out.println("----------------------------");
 
-        /* -----------------------------
-           2. Hashing Numbers (HashMap)
-           ----------------------------- */
+        // /* -----------------------------
+        //    2. Hashing Numbers (HashMap)
+        //    ----------------------------- */
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int num : arr) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-        }
+        // for (int num : arr) {
+        //     map.put(num, map.getOrDefault(num, 0) + 1);
+        // }
 
-        for (int q : queries) {
-            System.out.println("Frequency of " + q + " = " + map.getOrDefault(q, 0));
-        }
+        // for (int q : queries) {
+        //     System.out.println("Frequency of " + q + " = " + map.getOrDefault(q, 0));
+        // }
 
-        System.out.println("----------------------------");
+        // System.out.println("----------------------------");
 
-        /* -----------------------------
-           3. Hashing Characters
-           ----------------------------- */
+        // /* -----------------------------
+        //    3. Hashing Characters
+        //    ----------------------------- */
 
-        String s = "aabbccdde";
-        int[] charHash = new int[26];
+        // String s = "aabbccdde";
+        // int[] charHash = new int[26];
 
-        for (char ch : s.toCharArray()) {
-            charHash[ch - 'a']++;
-        }
+        // for (char ch : s.toCharArray()) {
+        //     charHash[ch - 'a']++;
+        // }
 
-        char[] charQueries = {'a', 'b', 'z'};
-        for (char c : charQueries) {
-            System.out.println("Frequency of " + c + " = " + charHash[c - 'a']);
-        }
+        // char[] charQueries = {'a', 'b', 'z'};
+        // for (char c : charQueries) {
+        //     System.out.println("Frequency of " + c + " = " + charHash[c - 'a']);
+        // }
 
-        System.out.println("----------------------------");
+        // System.out.println("----------------------------");
 
-        /* -----------------------------
-           4. Hashing Characters (HashMap)
-           ----------------------------- */
+        // /* -----------------------------
+        //    4. Hashing Characters (HashMap)
+        //    ----------------------------- */
 
-        HashMap<Character, Integer> charMap = new HashMap<>();
+        // HashMap<Character, Integer> charMap = new HashMap<>();
 
-        for (char ch : s.toCharArray()) {
-            charMap.put(ch, charMap.getOrDefault(ch, 0) + 1);
-        }
+        // for (char ch : s.toCharArray()) {
+        //     charMap.put(ch, charMap.getOrDefault(ch, 0) + 1);
+        // }
 
-        for (char c : charQueries) {
-            System.out.println("Frequency of " + c + " = " + charMap.getOrDefault(c, 0));
-        }
+        // for (char c : charQueries) {
+        //     System.out.println("Frequency of " + c + " = " + charMap.getOrDefault(c, 0));
+        // }
+
+        // Scanner s = new Scanner(System.in);
+        // System.out.print("Enter the value of n : ");
+        // int n = s.nextInt();
+        // int arr[] = new int[n];
+        // System.out.print("Enter values of array : ");
+        // for (int i=0;i<n;i++){
+        //     arr[i] = s.nextInt();
+        // }
+        // //precompute
+        // Map<Integer,Integer> map = new HashMap<>();
+        // for (int i=0;i<n;i++){
+        //     map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        // }
+        // System.out.print("Enter value of query : ");
+        // int query = s.nextInt();
+        // System.out.print("Enter numbers to find count in array : ");
+        // while (query >= 0){
+        //     int number = s.nextInt();
+        //     query--;
+        //     //fetch method
+        //     System.out.println(map.getOrDefault(number, 0));
+        // }
     }
 }
