d39d1757c572ca355ac9e9fb5b3941d8d658ee88
Shivani2965 <shivanidevanekar2965@gmail.com>
2026-06-09 18:08:24 +0530

    Time: 1 ms (96.61%), Space: 44.3 MB (64.55%) - LeetHub

--- 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.java
+++ 3591-check-if-any-element-has-prime-frequency/3591-check-if-any-element-has-prime-frequency.java
@@ -1,28 +1,30 @@
 class Solution {
-    public boolean checkPrimeFrequency(int[] nums) {
-        HashMap <Integer, Integer> counts = new HashMap<>();
-        for (int num :nums){
-            counts.put(num, counts.getOrDefault(num, 0)+1);
-
+    static boolean isPrime(int n) {
+        if(n <= 1) return false;
+        if(n == 2) return true;
+        if(n % 2 == 0) return false;
+        for(int i = 3; i*i <= n; i+=2) {
+            if(n % i == 0) return false;
         }
+        return true;
+    }
+    public boolean checkPrimeFrequency(int[] nums) {
+        int n = nums.length;
+        boolean visited[] = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(visited[i] == true) 
+            continue;
 
-        for (int count: counts.values()){
-            if(isPrime(count)){
-                return true;
+            int count = 1;
+            for(int j = i+1; j < n; j++) {
+                if(nums[i] == nums[j]) {
+                    visited[j] = true;
+                    count++;
+                }
             }
+            if(isPrime(count)) 
+              return true;
         }
         return false;
     }
-
-    public boolean isPrime(int n){
-    int c = 0;
-
-    for(int i = 1; i <= n; i++){
-        if(n % i == 0){
-            c++;
-        }
-    }
-
-    return c == 2;
-}
 }
